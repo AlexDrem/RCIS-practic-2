1b639b9204a348beeb016a4320ee91ab6b078c12
AlexDrem <119021230+AlexDrem@users.noreply.github.com>
2023-03-04 07:23:31 +0700

    Add files via upload

---
+++ 2.3.1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Worker
+    {
+        public string name, surname;
+        public int days, rate;
+
+        public void GetSalary()
+        {
+            Console.WriteLine($"зпшка -  {days * rate}");
+        }
+    }
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Worker dima = new Worker();
+            dima.name = "Dimka";
+            dima.surname = "Kuplinov";
+            dima.days = 4;
+            dima.rate = 5000;
+            dima.GetSalary();
+        }
+    }
+}
