4d59602804a7d6b01e646c85346916563d7a4b48
AlexDrem <119021230+AlexDrem@users.noreply.github.com>
2023-03-04 08:43:35 +0700

    Add files via upload

---
+++ 2.1.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string str;
+            int[] nums = { 2, 4, 1, 3, 1, 4, 3, 2, 1, 3 };
+            bool a = false;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                {
+                    a = true;
+                }
+            }
+
+            if (a == false)
+            {
+                str = "False";
+            }
+            else
+            {
+                str = "True";
+            }
+
+            Console.WriteLine(str);
+        }
+    }
+}
