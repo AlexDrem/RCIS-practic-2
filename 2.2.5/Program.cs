d1761f08837676079f15d9d57ef89910c71dd3e2
AlexDrem <119021230+AlexDrem@users.noreply.github.com>
2023-03-04 07:08:26 +0700

    Add files via upload

---
+++ 2.2.5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _2._5
+{
+
+    class chtoto {
+        private string name;
+        private string surname;
+
+        public chtoto() { name = "Неизвестно"; surname = "Неизвестно"; }
+        public chtoto(string name,string surname) { this.name = name; this.surname = surname; }
+
+        public void GetPrint()
+        {
+            Console.WriteLine($"Имя: {name}  Фамилия: {surname}");
+        }
+
+        ~chtoto()
+        {
+            Console.WriteLine($"{name} has deleted");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            chtoto d1 = new chtoto("Dmitry","Kuplinov");
+            chtoto d2 = new chtoto();
+            d1.GetPrint();
+            d2.GetPrint();
+            Console.WriteLine("Димка лучший");
+
+
+        }
+    }
+}
